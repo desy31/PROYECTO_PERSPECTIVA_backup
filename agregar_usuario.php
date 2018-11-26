69012dc2ebf424c06828b7f835e6f32aa88ddd95
desy31 <45342624+desy31@users.noreply.github.com>
2018-11-25 19:08:08 -0600

    modificar usuario

--- agregar_usuario.php
+++ agregar_usuario.php
@@ -6,12 +6,12 @@
 	if ($_POST['nombre'] && $_POST['usuario'] && $_POST['password'] && $_POST['rol']) {
 		$accion = "";
 
-		if ($_SESSION["usuario_actual_rol"] === 'admin' && $_POST['Guardar']) {
+		if ($_SESSION["usuario_actual_rol"] === 'admin' && isset($_POST['Guardar'])) {
 			$accion = "Ingresado";
 			$sql = "INSERT INTO usuario (`IdUsuario`, `Usuario`, `Password`, `Rol`, `Nombre`) VALUES (NULL,'".$_POST['usuario']."','".$_POST['password']."','".$_POST['rol']."','".$_POST['nombre']."')";
-		}else if ($_POST['Modificar']) {
+		}else if ($_SESSION["usuario_actual_rol"] === 'admin' && isset($_POST['Modificar'])) {
 			$accion = "Modificado";
-			$sql = "UPDATE `usuario` SET `IdUsuario`='".$_POST['id']."',`Usuario`='".$_POST['usuario']."',`Password`='".$_POST['password']."',`Rol`='".$_POST['rol']."',`Nombre`='".$_POST['nombre']."' WHERE IdUsuario = '".$_POST['id']."'";
+			$sql = "UPDATE `usuario` SET `IdUsuario`='".$_POST['idusuario']."',`Usuario`='".$_POST['usuario']."',`Password`='".$_POST['password']."',`Rol`='".$_POST['rol']."',`Nombre`='".$_POST['nombre']."' WHERE IdUsuario = '".$_POST['idusuario']."'";
 		}
 
 		if (mysql_query($sql, $con)) {
@@ -20,8 +20,8 @@
 		}else{
 			echo "<script> alert('Error al '".$accion."' usuario');</script>";
 		}
-		// echo "<script>
-		// 	location.replace('usuarios.php');
-		// 	</script>";
+		echo "<script>
+			location.replace('usuarios.php');
+			</script>";
 	}
 ?>
